e204f142a5b060a4ab53b3e3d9ab893d7408c490
fdt <fdt@fdtdemacbook-pro.local>
2017-04-27 10:34:22 +0800

    Initial Commit

---
+++ TextLanuch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextLanuch
+//
+//  Created by fdt on 2017/4/27.
+//  Copyright © 2017年 fdt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextLanuch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextLanuch
+//
+//  Created by fdt on 2017/4/27.
+//  Copyright © 2017年 fdt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
